08cfad5d5bd9efb2657ced76462c5262979d3c83
oligeon-dev <oligeon.dev@gmail.com>
2023-06-06 09:09:41 +0900

    become pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    useCredentials: true,
+    manifest: {
+      name: 'aaaaaaaaaaaaaaaaaaaa',
+      short_name: 'bbbbbbbbbbbbbbbbbbbbbb',
+
+    },
+    registerType: 'autoUpdate'
+  })
+  ],
 })
