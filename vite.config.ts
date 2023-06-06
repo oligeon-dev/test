445d728f2bb02a924e91daa911787ea3a079c5dc
oligeon-dev <oligeon.dev@gmail.com>
2023-06-06 09:28:15 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  VitePWA({
-    useCredentials: true,
-    manifest: {
-      name: 'aaaaaaaaaaaaaaaaaaaa',
-      short_name: 'bbbbbbbbbbbbbbbbbbbbbb',
-
-    },
-    injectRegister: 'auto',
-    registerType: 'autoUpdate'
-  })
+  plugins: [
+    react(),
+    VitePWA({
+      useCredentials: true,
+      manifest: {
+        name: "aaaaaaaaaaaaaaaaaaaa",
+        short_name: "bbbbbbbbbbbbbbbbbbbbbb",
+        icons: [
+          {
+            src: "test.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+    }),
   ],
-})
+});
