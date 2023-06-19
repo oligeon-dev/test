6ea4d80a0999821ef6b3191f64b579d407f3fadc
oligeon-dev <oligeon.dev@gmail.com>
2023-06-20 00:39:57 +0900

    android version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const userAgentData = navigator.userAgentData;
+  const [version, setVersion] = useState("");
+  const userAgent = navigator.userAgent;
+  console.log(userAgent);
 
-  return <pre>{JSON.stringify(userAgentData, null, 2)}</pre>;
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const androidVersion = extractAndroidVersion(userAgent);
+    console.log("Android Version: ", androidVersion);
+    if (androidVersion) {
+      setVersion(androidVersion);
+    }
+  }, []);
+
+  const extractAndroidVersion = (userAgent: string) => {
+    const match = userAgent.match(/Android\s([0-9\.]*)/);
+    if (match) {
+      return match[1];
+    }
+    return null;
+  };
+  return <div>{version}</div>;
 }
 
 export default App;
