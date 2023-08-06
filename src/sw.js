77c134e14f9bc610af53757986eb40df6eef32d4
oligeon-dev <oligeon.dev@gmail.com>
2023-08-06 15:11:17 +0900

    test

--- src/sw.js
+++ src/sw.js
@@ -1,15 +1,18 @@
 // sw.js
 
-const VERSION = "v1";
+const VERSION = "v2";
 
 self.addEventListener("install", (event) => {
   // インストール処理
+  console.log("install");
 });
 
 self.addEventListener("activate", (event) => {
   // アクティベーション処理
+  console.log("activate");
 });
 
 self.addEventListener("fetch", (event) => {
   // ネットワークリクエストのインターセプトなど
+  console.log("fetch");
 });
